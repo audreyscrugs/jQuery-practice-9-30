7d4f97b729a50091cb54a99688118a35f3ed644f
Audrey Scrugham & Midori Bowen <student@epicodus.com>
2015-09-30 19:22:32 -0700

    Add hide/show function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,8 @@
 $(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I already told you! THIS is a header!");
-  });
 
   $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").dblclick(function() {
-    alert("This is an image.");
-  });
-
-  $("ul").hover(function() {
-    alert("This is a walrus");
+    $(".walrus-showing").show();
+    $(".walrus-hidden").hide();
   });
 
 });
