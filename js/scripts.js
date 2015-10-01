980e6ae12e87d4aca26f811781c60815154029d1
Audrey Scrugham & Midori Bowen <student@epicodus.com>
2015-09-30 18:49:43 -0700

    Add additional arguments and commit jquery file

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 $(function() {
   $("h1").click(function() {
     alert("This is a header.");
+    alert("I already told you! THIS is a header!");
   });
 
   $("p").click(function() {
     alert("This is a paragraph.");
   });
 
-  $("img").click(function() {
+  $("img").dblclick(function() {
     alert("This is an image.");
   });
-  
+
+  $("ul").hover(function() {
+    alert("This is a walrus");
+  });
+
 });
